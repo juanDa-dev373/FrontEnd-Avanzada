3ccb3bf5a74e025053adb20200a634b2a2d1e02d
jdccboom <juand.cortesc1@uqvirtual.edu.co>
2024-05-25 20:32:32 -0500

    se cambio enviroments.ts

--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -1,5 +1,5 @@
 export const enviroments={
-    urlHost:"http://localhost:8083/",
-    urlApi:"http://localhost:8083",
+    urlHost:"https://programaci-n-avanzada.onrender.com/",
+    urlApi:"https://programaci-n-avanzada.onrender.com",
     accessToken:'pk.eyJ1IjoianVhbmZvbnNlY2EyMDAzIiwiYSI6ImNsdmlweHVxaDFpZmUya21naXF5bnJrdW4ifQ.IANrJ9QI3kdPt2Qlh8vh-g'
 }
